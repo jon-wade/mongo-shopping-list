6470d91c9857cf4c10989fa1f6a9dd782d13bf76
jon-wade <jonwadeuk@gmail.com>
2016-05-25 07:00:00 +0100

    change app.listen config

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use('*', function(req, res) {
     res.status(404).json({ message: 'Not Found' });
 });
 
-app.listen(8080, function() {
+app.listen(process.env.PORT || 8080, function() {
     console.log('Listening on port 8080');
 });
 
